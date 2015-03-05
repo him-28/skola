05306341c7c35b83f1fb403e2abba6d9e0d951db
Pavel Ondračka <pavel.ondracka@gmail.com>
2015-03-05 14:11:17 +0100

    pb071: final version for first homework

--- PB071/du1/calc.c
+++ PB071/du1/calc.c
@@ -1,15 +1,22 @@
+/**
+ * @author  Pavel Ondračka <pavel.ondracka@gmail.com>
+ * @version 1.0
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 
-int npow(int a, int b){
-   int ret = 1;
-   for (int i = 0; i < b; i++)
+long npow(long a, long b){
+   long ret = 1;
+   for (long i = 0; i < b; i++)
       ret *= a;
    return ret;
 }
 
-int fact(int a){
-   int ret = 1;
+long fact(long a){
+   if(a < 0)
+      return -1;
+   long ret = 1;
    while(a > 0){
       ret *= a;
       a--;
@@ -17,8 +24,8 @@ int fact(int a){
    return ret;
 }
 
-int sum(int num, int *data){
-   int ret = 0;
+long sum(long num, long *data){
+   long ret = 0;
    while(num != 0){
       ret += data[num-1];
       num--;
@@ -26,9 +33,11 @@ int sum(int num, int *data){
    return ret;
 }
 
-float aver(int num, int *data){
-   int sum = 0;
-   int n = num;
+float aver(long num, long *data){
+   if(num == 0)
+      return 0.0;
+   long sum = 0;
+   long n = num;
    while(num != 0){
       sum += data[num-1];
       num--;
@@ -36,16 +45,18 @@ float aver(int num, int *data){
    return (float)sum / n;
 }
 
-int binom(int top, int bot){
+long binom(long top, long bot){
+   if(top < bot || bot < 0)
+      return -1;
    return fact(top) / (fact(bot) * fact(top - bot));
 }
 
-int prime(int a){
+long prime(long a){
    if(a == 2)
       return 1;
    if(a == 1 || a % 2 == 0)
       return 0;
-   for(int i = 3; i <= a / 2; i += 2)
+   for(long i = 3; i <= a / 2; i += 2)
       if((a % i) == 0)
          return 0;
    return 1;
@@ -58,92 +69,102 @@ int main(){
 
    while(scanf("%c", &op) != EOF){
 
-      int n1, n2;
+      long n1, n2;
       float fn1, fn2;
-      int *data;
+      long *data;
 
       switch(op){
 
          case '+':
-            scanf("%i %i", &n1, &n2);
-            printf("# %i\n", n1 + n2);
+            scanf("%li %li", &n1, &n2);
+            printf("# %li\n", n1 + n2);
             break;
 
          case '-':
-            scanf("%i %i", &n1, &n2);
-            printf("# %i\n", n1 - n2);
+            scanf("%li %li", &n1, &n2);
+            printf("# %li\n", n1 - n2);
             break;
 
          case '/':
             scanf("%f %f", &fn1, &fn2);
-            printf("# %.3f\n", fn1 / fn2);
+            printf("# %.2f\n", fn1 / fn2);
             break;
 
          case 'd':
-            scanf("%i %i", &n1, &n2);
-            printf("# %i\n", n1 / n2);
+            scanf("%li %li", &n1, &n2);
+            printf("# %li\n", n1 / n2);
             break;
 
          case 'm':
-            scanf("%i %i", &n1, &n2);
-            printf("# %i\n", n1 % n2);
+            scanf("%li %li", &n1, &n2);
+            printf("# %li\n", n1 % n2);
             break;
 
          case '*':
-            scanf("%i %i", &n1, &n2);
-            printf("# %i\n", n1 * n2);
+            scanf("%li %li", &n1, &n2);
+            printf("# %li\n", n1 * n2);
             break;
 
          case '^':
-            scanf("%i %i", &n1, &n2);
-            printf("# %i\n", npow(n1, n2));
+            scanf("%li %li", &n1, &n2);
+            printf("# %li\n", npow(n1, n2));
             break;
 
          case '!':
-            scanf("%i", &n1);
-            printf("# %i\n", fact(n1));
+            scanf("%li", &n1);
+            printf("# %li\n", fact(n1));
             break;
 
          case 's':
-            scanf("%i", &n1);
-            if((data = (int*)malloc(n1 * sizeof(int))) == NULL){
+            scanf("%li", &n1);
+            if((data = (long*)malloc(n1 * sizeof(long))) == NULL){
                printf("failed to allocate memory\n");
                exit(1);
             }
-            for(int i = 0; i < n1; i++)
-               scanf("%i", &data[i]);
-            printf("# %i\n", sum(n1, data));
+            for(long i = 0; i < n1; i++)
+               scanf("%li", &data[i]);
+            printf("# %li\n", sum(n1, data));
             free(data);
             break;
 
          case 'a':
-            scanf("%i", &n1);
-            if((data = (int*)malloc(n1 * sizeof(int))) == NULL){
+            scanf("%li", &n1);
+            if((data = (long*)malloc(n1 * sizeof(long))) == NULL){
                printf("failed to allocate memory\n");
                exit(1);
             }
-            for(int i = 0; i < n1; i++)
-               scanf("%i", &data[i]);
-            printf("# %.3f\n", aver(n1, data));
+            for(long i = 0; i < n1; i++)
+               scanf("%li", &data[i]);
+            printf("# %.2f\n", aver(n1, data));
             free(data);
             break;
 
          case 'c':
-            scanf("%i %i", &n1, &n2);
-            printf("# %i\n", binom(n1, n2));
+            scanf("%li %li", &n1, &n2);
+            printf("# %li\n", binom(n1, n2));
             break;
 
          case 'p':
-            scanf("%i", &n1);
-            printf("# %s\n", prime(n1) ? "yes" : "no");
+            scanf("%li", &n1);
+            printf("# %s\n", prime(n1) ? "y" : "n");
             break;
+   
+         case 'q':
+            return 0;
 
          default:
             printf("Unknown operation %c\n", op);
             break;
       }
-   while(getchar() != '\n');
-     printf("> ");
+
+   char c;
+   while((c = getchar())){
+      if(c == '\n')
+         break;
+      else if(c == EOF)
+         return 0;
+   }
+   printf("> ");
    }
 
    return 0;
