013c30f74753a5ad6977479ae16def2322cebc61
Pavel Ondračka <pavel.ondracka@gmail.com>
2015-04-06 21:39:17 +0200

    PB071: small fix for third homework

--- PB071/du3/main.c
+++ PB071/du3/main.c
@@ -33,7 +33,8 @@ void queue_add (struct queue* container, const enum instruction_type type, const
 }
 
 void print_help(){
-};
+   
+}
 
 void parse_args(int argc, char *argv[], int* batch_size){
    my_assert(argc > 1 && "Number of arguments too low");
