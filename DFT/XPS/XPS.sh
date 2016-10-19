47ac9b86983fe0f9c5e80dacff5ed8975934ebb6
Pavel Ondračka <pavel.ondracka@gmail.com>
2016-10-19 14:58:12 +0200

    DFT-XPS: updates to XPS.sh script

--- DFT/XPS/XPS.sh
+++ DFT/XPS/XPS.sh
@@ -18,14 +18,15 @@
 #TODO: add the delta-SCF method
 #TODO: check multiplicity and error if not one
 #TODO: add the print option
+#TODO: add better command line arguments handling
 
 function help {
    echo "Script to calculate core electron shifts using Wien2k"
    echo ""
-   echo "Usage: XPS.sh [atoms]:[levels] [atoms:levels]"
+   echo "Usage: XPS.sh [atoms:levels] [atoms:levels] ..."
    echo "atoms are selected by their number from struct file"
-   echo "multiple atoms can be selected using ',' or '-' e.g. 1,2,5 or 5-10"
-   echo "the same syntax goes for core levels"
+   echo "multiple atoms can be selected using ',' or '-' eg. 1,2,5"
+   echo "or 5-10,12-17, the same syntax goes for core levels"
    echo ""
    echo "-h print this help and exit"
    echo "-p print list of atoms with core levels"
@@ -41,6 +42,8 @@ then
    exit
 fi
 
+DEBUG=0
+
 edgenames=("" "1S" "2S" "2PP" "2P")
 
 CASE=$(basename $PWD)
@@ -48,6 +51,8 @@ cd $PWD
 
 line=1
 atoms=0
+
+method="forb" # slater dscf forb
 
 while ((1))
 do
@@ -66,88 +71,122 @@ do
    ((line++))
 done
 
-for ((i=1; i<=atoms; i++))
-do
-   echo $i ${at_names[$i]} ${atoms[$i]} 
-done
+if [ "$DEBUG" == 1 ]
+then
+   for ((i=1; i<=atoms; i++))
+   do
+      echo $i ${at_names[$i]} ${atoms[$i]}
+   done
+fi
 
+edges=()
 for arg in $@
 do
+   # divide into atomic part and edge part
    selected_atoms=$(echo $arg | cut -d ":" -f1)
    selected_atoms=$(echo $selected_atoms | sed 's/,/ /g')
    selected_edges=$(echo $arg | cut -d ":" -f2)
    selected_edges=$(echo $selected_edges | sed 's/,/ /g')
 
-   expanded_atoms=()
+   # expand the edges into string of integers FIXME: function
+   expanded_edges=""
+   for i in $selected_edges
+   do
+      if echo $i | grep "-" > /dev/null
+      then
+         min=$(echo $i | cut -d "-" -f1)
+         max=$(echo $i | cut -d "-" -f2)
+         expanded_edges+=" $(seq $min $max)"
+         continue
+      fi
+      expanded_edges+=" $i"
+   done
 
+   # expand the atoms into string of integers FIXME: function
+   expanded_atoms=""
    for i in $selected_atoms
    do
       if echo $i | grep "-" > /dev/null
       then
          min=$(echo $i | cut -d "-" -f1)
          max=$(echo $i | cut -d "-" -f2)
-         expanded_atoms+=($(seq $min $max))
+         expanded_atoms+=" $(seq $min $max)"
          continue
       fi
-      expanded_atoms+=($i)
+      expanded_atoms+=" $i"
+   done
+
+   # add to the edge list
+   for i in $expanded_atoms
+   do
+      edge[$i]+=" $expanded_edges"
+      edge[$i]=$(echo "${edge[$i]}" | tr ' ' '\n' | sort -n | uniq | tr '\n' ' ')
    done
 done
 
-echo ${expanded_atoms[@]}
-
-for atom in ${expanded_atoms[@]}
+# go over all combination and calculate the binding energies
+for ((atom=1; atom<=atoms; atom++))
 do
-   if ((atom > atoms))
-   then
-      echo "atom number $atom not present"
-      continue
-   fi
 
-   for edge in $selected_edges
+   for edge in ${edge[$atom]}
    do
+      if [ "$DEBUG" == 1 ]
+      then
+         echo $i-$edge
+      fi
+      
       if ((edge > atoms[atom]))
       then
-         echo "edge $edge for atom $atom not present"
+         echo "Warning: edge $edge for atom $atom not present"
          continue
       fi
 
-      touch .XPSlock-$atom-$edge
+      greppatern=$(printf "%-3s%0*d" "${edgenames[$edge]}" 3 $atom)
 
-      (
-         save_lapw -a -d $atom-$edge $atom-$edge > /dev/null
-         cd $atom-$edge
-
-         ls ../.lcore > /dev/null && touch .lcore
-         sed -i 's/ 0.0/-0.5/g' $atom-$edge.inm
-         incline=$((line[$atom]+edge))
-         awk -F "," -v n=$incline '{if (NR==n){printf "%1i,%1i,%.1f\n", $1, $2, $3-0.5} else print}' $atom-$edge.inc > tmp.inc
-         mv tmp.inc $atom-$edge.inc
-
-         task_id=$(qsub -A OPEN-6-17 -q qprod -l select=1:ncpus=24,walltime=30:00:00 ~/scripts/kpar_xnodes_mpi)
-
-	 #this could be probably done in a smarter way, with no need to spawn lots of subshells
-         while qstat -a -u ondracka | grep $task_id > /dev/null
-         do
-            sleep 120
-         done
-
-         greppatern=$(printf "%-3s%0*d" "${edgenames[$edge]}" 3 $atom)
-         ecore=$(grep "$greppatern" $atom-$edge.scfc | grep -o "\-[0-9]*\.[0-9]*")
-         efermi=$(grep \:FER $atom-$edge.scf2 | grep -Eo "\-?[0-9]+\.[0-9]+")
-
+      if [ "$method" == "forb" ]
+      then
+         ecore=$(grep "$greppatern" $CASE.scfc | grep -o "\-[0-9]*\.[0-9]*")
+         efermi=$(grep \:FER $CASE.scf2 | grep -Eo "\-?[0-9]+\.[0-9]+")
          energy=$(echo "$efermi - $ecore" | bc)
-         echo $atom ${at_names[$atom]} $edge ${edgenames[$edge]} $ecore $efermi $energy >> ../XPS-shifts.txt
+         echo $atom ${at_names[$atom]} $edge ${edgenames[$edge]} $ecore $efermi $energy >> XPS-shifts-$method.txt
+      elif [ "$method" == "slater" ] || [ "$method" == "slater" ]
+      then
+         touch .XPSlock-$atom-$edge
 
-         cd ..
+         (
+            save_lapw -a -d $atom-$edge $atom-$edge > /dev/null
+            cd $atom-$edge
 
-	 rm .XPSlock-$atom-$edge
-      )&
+            ls ../.lcore > /dev/null && touch .lcore
+            sed -i 's/ 0.0/-0.5/g' $atom-$edge.inm
+            incline=$((line[$atom]+edge))
+            awk -F "," -v n=$incline '{if (NR==n){printf "%1i,%1i,%.1f\n", $1, $2, $3-0.5} else print}' $atom-$edge.inc > tmp.inc
+            mv tmp.inc $atom-$edge.inc
 
-      # limit number of processes
-      while [ $(ls .XPSlock* | wc -l) -gt "80" ]
-      do
-         sleep 60
-      done
+            #task_id=$(qsub -A OPEN-6-17 -q qprod -l select=1:ncpus=24,walltime=30:00:00 ~/scripts/kpar_xnodes_mpi)
+
+            #this could be probably done in a smarter way, with no need to spawn lots of subshells
+            while qstat -a -u ondracka | grep $task_id > /dev/null
+            do
+               sleep 120
+            done
+
+            ecore=$(grep "$greppatern" $atom-$edge.scfc | grep -o "\-[0-9]*\.[0-9]*")
+            efermi=$(grep \:FER $atom-$edge.scf2 | grep -Eo "\-?[0-9]+\.[0-9]+")
+            energy=$(echo "$efermi - $ecore" | bc)
+            echo $atom ${at_names[$atom]} $edge ${edgenames[$edge]} $ecore $efermi $energy >> ../XPS-shifts-$method.txt
+
+            cd ..
+
+            rm .XPSlock-$atom-$edge
+         )&
+
+         # limit number of processes
+         while [ $(ls .XPSlock* | wc -l) -gt "80" ]
+         do
+            sleep 60
+         done
+      fi
    done
 
 done
